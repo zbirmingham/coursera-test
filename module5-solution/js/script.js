880f73e257fbcc25120572ffc8c2c03a0bbf17a0
DesigniMac2 <DesigniMac2@Zacks-iMac.local>
2021-09-13 17:42:05 -0400

    fix for solution

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -88,7 +88,6 @@ $ajaxUtils.sendGetRequest(
     return response;
   },
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
-  console.log(responseText);
 });
 // *** finish **
 
@@ -106,7 +105,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -120,7 +119,8 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+      var catergoryPassToHomepage = "'" + chosenCategoryShortName + "'";
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", catergoryPassToHomepage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
